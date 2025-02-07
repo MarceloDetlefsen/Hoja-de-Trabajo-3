b812d157e1e42102619fd289353e1c38a482efa8
Marcelo Detlefsen <83415711+MarceloDetlefsen@users.noreply.github.com>
2025-02-07 00:06:08 -0600

    Se crea la estructura básica del programa.

---
+++ Sorting.java
@@ -0,0 +1,39 @@
+public class Sorting
+{
+    public static void main(String[] args) {
+        
+    }
+
+    public static void generadorNumeros(String nombreArchivo, int cantidadNumeros) {
+        // Generar un archivo con cantidadNumeros números aleatorios
+    }
+
+    public static int[] leerNumeros(String nombreArchivo) {
+        // Leer los números de un archivo y devolverlos en un arreglo
+        return null;
+    }
+
+    public static void medidorSort(String nombreArchivo) {
+        // Medir el tiempo que toma ordenar un arreglo de números
+    } 
+
+    public static void insertionSort(int[] arreglo) {
+        // Ordenar el arreglo usando el algoritmo de Insertion Sort
+    }
+
+    public static void mergeSort(int[] arreglo) {
+        // Ordenar el arreglo usando el algoritmo de Merge Sort
+    }
+
+    public static void quickSort(int[] arreglo) {
+        // Ordenar el arreglo usando el algoritmo de Quick Sort
+    }
+
+    public static void radixSort(int[] arreglo) {
+        // Ordenar el arreglo usando el algoritmo de Radix Sort
+    }
+
+    public static void timSort(int[] arreglo) {
+        // Ordenar el arreglo usando el algoritmo de Tim Sort
+    }
+}
