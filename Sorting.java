bc38bb9bfb18a43ce6605cd511f3013bd359d910
Luis Pedro Hernandez <lphm0105@gmail.com>
2025-02-08 17:28:02 -0600

    QuickSort, RadixSort y Main

--- Sorting.java
+++ Sorting.java
@@ -8,12 +8,59 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Sorting
 {
     public static void main(String[] args) {
-        //Generar los numeros e imprimirlos para demostrar que estan desordenados,
-        //luego ir a un menú donde se seleccione con que método se quiere ordenar y volver a imprimirlos una vez termine el sorting.
+        // Generar los números
+        generadorNumeros();
+        
+        // Leer los números del archivo
+        String archivo = "numeros.txt";
+        int[] numeros = leerNumeros(archivo);
+        
+        // Imprimir los números desordenados
+        System.out.println("Números desordenados:");
+        System.out.println(Arrays.toString(numeros));
+        
+        // Menú para seleccionar el algoritmo de ordenamiento
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nSelecciona el método de ordenamiento:");
+        System.out.println("1. InsertionSort");
+        System.out.println("2. MergeSort");
+        System.out.println("3. QuickSort");
+        System.out.println("4. RadixSort");
+        System.out.println("5. BucketSort");
+        int opcion = scanner.nextInt();
+        
+        // Ordenar dependiendo de la opción seleccionada
+        switch (opcion) {
+            case 1:
+                break;
+            case 2:
+                mergeSort(numeros, 0, numeros.length - 1);
+                break;
+            case 3:
+                quickSort(numeros, 0, numeros.length - 1);
+                break;
+            case 4:
+                radixSort(numeros);
+                break;
+            case 5:
+                bucketSort(numeros);
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
+        
+        // Imprimir los números ordenados
+        System.out.println("\nNúmeros ordenados:");
+        System.out.println(Arrays.toString(numeros));
+        
+        // Guardar los números ordenados en un archivo
+        guardarNumeros("numeros_ordenados.txt", numeros);
     }
 
     // Generar un archivo con cantidadNumeros números aleatorios
@@ -35,7 +82,7 @@ public class Sorting
     }
 
     //Leer los números de un archivo y devolverlos en un arreglo
-    public static List<Integer> leerNumeros(String nombreArchivo) {
+    public static int[] leerNumeros(String nombreArchivo) {
         List<Integer> numeros = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
             String linea;
@@ -46,17 +93,21 @@ public class Sorting
         catch (IOException e) {
             System.out.println("Error al leer el archivo " + nombreArchivo);
         }
-        return numeros;
+
+        int[] arreglo = new int[numeros.size()];
+        for (int i = 0; i < numeros.size(); i++) {
+            arreglo[i] = numeros.get(i);
+        }
+        return arreglo;
     }
 
     // Guardar los números en un archivo
-    public static void guardarNumeros(String nombreArchivo, List<Integer> numeros) {
+    public static void guardarNumeros(String nombreArchivo, int[] arreglo) {
         try (FileWriter file = new FileWriter(nombreArchivo)) {
-            for (int numero : numeros) {
+            for (int numero : arreglo) {
                 file.write(numero + "\n");
             }
-        } 
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error al escribir en el archivo " + nombreArchivo);
         }
     }
@@ -87,13 +138,73 @@ public class Sorting
     }
 
     // Ordenar el arreglo usando el algoritmo de Quick Sort
-    public static void quickSort(int[] arreglo, int bajo, int alto) {
-        
+    public static void quickSort(int[] arreglo, int izq, int der) {
+        if (izq < der) {
+            // Obtiene el índice del pivote
+            int pivote = particion(arreglo, izq, der);
+            // Ordena las dos mitades recursivamente
+            quickSort(arreglo, izq, pivote - 1);
+            quickSort(arreglo, pivote + 1, der);
+        }
     }
+
+    public static int particion(int[] arreglo, int izq, int der) {
+        // Selecciona el último elemento como pivote
+        int pivote = arreglo[der];
+        int i = izq - 1; // Indice del elemento más pequeño
+
+        // Recorre el arreglo y organiza los elementos en torno al pivote
+        for (int j = izq; j < der; j++) {
+            // Si el elemento actual es menor o igual al pivote
+            if (arreglo[j] <= pivote) {
+                i++;
+                // Intercambia los elementos
+                cambio(arreglo, i, j);
+            }
+        }
+        cambio(arreglo, i + 1, der);
+        return i + 1; // Devuelve el índice del pivote
+    }
+
+    public static void cambio(int[] arreglo, int i, int j) {
+        int temp = arreglo[i];
+        arreglo[i] = arreglo[j];
+        arreglo[j] = temp;
+    }
+    
 
     // Ordenar el arreglo usando el algoritmo de Radix Sort
     public static void radixSort(int[] arreglo) {
+        // Loop que crea el array de ordenamiento
+        ArrayList<ArrayList<Integer>> radixArray = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < 10; i++){
+            radixArray.add(new ArrayList<Integer>());
+        }
 
+        // Encontrar el valor máximo del arreglo
+        int valorMax = Arrays.stream(arreglo).max().getAsInt();;
+        int exponente = 1;
+
+        while (valorMax / exponente > 0) {
+            // Separar los elementos en buckets según el digito
+            for (int i = 0; i < arreglo.length; i++) {
+                int numero = arreglo[i];
+                int digito = (numero / exponente) % 10;
+                radixArray.get(digito).add(numero);
+            }
+
+            // Reordenar el arreglo
+            int index = 0;
+            for (int o = 0; o < 10; o++) {
+                for (int num : radixArray.get(o)) {
+                    arreglo[index++] = num;
+                }
+                radixArray.get(o).clear();
+            }
+
+            // Pasar al siguiente digito
+            exponente *= 10;
+        }
     }
 
     // Ordenar el arreglo usando el algoritmo de Bucket Sort
